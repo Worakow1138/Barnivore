780fad64135bd5f5b2e064377de408cd1ef528d7
Christopher Diamond-Jones <christopher.jones1138@gmail.com>
2023-05-22 13:39:24 -0400

    Commit initial info
    Create gitignore file
    
    Create dockerfile
    
    Create run_tests.sh script

---
+++ run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ensure that GOOGLE_APPLICATION_CREDENTIALS is set as an ENV variable in Cloud Run Job
+gcloud auth activate-service-account --key-file GOOGLE_APPLICATION_CREDENTIALS
+moonrise . $@
+export TZ="America/New_York"
+current_date_time=$(date '+%Y-%m-%d_%H:%M:%S')
+gsutil -m cp -r reports gs://barnivore_test_reports/reports_$current_date_time
