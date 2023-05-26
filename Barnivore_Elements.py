391fc9cc59ccdb0cb5d854c40d13d609d9c0f908
Christopher Diamond-Jones <christopher.jones1138@gmail.com>
2023-05-26 15:39:53 -0400

    Update Barnivore_Elements.py
    Add data for AskACompanyPage class

--- Barnivore_Elements.py
+++ Barnivore_Elements.py
@@ -105,8 +105,88 @@ class LiquorPage:
 class AskACompanyPage:
 
     header_title = "Ask a Company"
-    def __init__(self):
-        self.list_widget = ListElements(self.header_title)
+    main_content_element = "div#content"
+    question_element = "#questions > div.bodycopy"
+    vegan_response_element = "#vegan_responses > div.bodycopy"
+    non_vegan_response_element = "#nonvegan_responses > div.bodycopy"
+    question_language_selector = "#questions > select"
+    brand_name_input = "#txtBrandName"
+    sender_name_input = "#txtYourName"
+    non_vegan_response_language_selector = "#nonvegan_responses > select"
+    vegan_response_language_selector = "#vegan_responses > select"
+    ask_a_company_paragraph = """
+    Ask a company
+    Barnivore relies on input from readers like you to check (and double check!) with your favourite drink makers to see what's in their products.
+    Checking with a company is simple. All you need to do is send them an email with one of our standard questions (see below) and forward us the response so we can add it to the list and help the rest of the world enjoy vegan drinks!
+    If you get a response that isn't awesome, please (politely!) let them know how you feel! We've got a sample response you can use here.
+    """.strip().replace("    ","")
+    the_question_title = """
+    The Question
+    Here's the Standard Question we prefer everyone uses:
+    """.strip().replace("    ","")
+    the_response_title = """
+    The Response
+    If the company responds that everything's vegan friendly, be sure to thank them and let them know how important this is to you!
+    If, on the other hand, the company isn't totally vegan friendly, it can help to send them a polite reminder. Here's an example you can use:
+    IF THE PRODUCTS ARE NOT VEGAN:
+    """.strip().replace("    ","")
+
+    def __init__(self, language="English", brand_name = "[BRAND NAME]", your_name = "[YOUR NAME]"):
+        self.brand_name = brand_name
+        self.your_name = your_name
+
+        self.the_question_dict = {
+        "English": """Hi, I'm helping to update a global online directory (over 54,000 entries so far) of vegan-friendly alcohol, http://www.barnivore.com/, and I was hoping you could provide some information about """+brand_name+""".
+        Does """+self.brand_name+""" contain any animal ingredients (such as milk/lactose/casein, eggs, cochineal, honey, animal-derived enzymes, etc) or are animal products used in the processing/filtration of the product (such as isinglass, gelatin, etc)?
+        Also, is your product manufactured or bottled anywhere else in the world (by a sub-licensee, for instance) that might use a different processing system, thus making them unsuitable for vegans?
+        Thanks,
+        """+self.your_name+"""
+        """,
+        "French": """Bonjour,
+        J'aide à la mise à jour d'un site Internet listant des boissons alcoolisées végétaliennes (www.barnivore.com ; plus de 54 000 inscriptions) et j'espérais que vous puissiez m’informer davantage sur vos produits.
+        
+        Est-ce que vos produits contiennent de quelconques produits d'origine animale (comme du lait/caséine/lactose, de la cochenille, des œufs, du miel, etc.) ou est-ce qu'un quelconque produit d'origine animale est utilisé dans la fabrication/filtration des produits (par exemple : du ichtyocolle, de la gélatine, etc.) ?
+        
+        Outre cela, vos produits sont-ils fabriqués ou embouteillés ailleurs dans le monde (sous licence, par exemple) qui pourrait utiliser un système de traitement différent, les rendant ainsi inadaptés aux végétaliens?
+        Je vous remercie,
+        """+self.your_name+"""
+        (Thanks, Marie-Sarah, Monique, and Greg!)
+        """
+        }
+
+        self.non_vegan_dict = {
+        "English": """Thank you for the response. I am very disappointed to hear that you use animal products in your products. While I understand that these ingredients may not be present in the final product, I wish to avoid products that use these ingredients at all, as do many others.
+        My understanding is that there are fining alternatives that do not utilize animal products, as evidenced by the wide variety of vegan-friendly companies listed on http://www.barnivore.com. Many wineries, breweries, and distillers are now using these techniques, and I would encourage you to look into these alternatives so that we can enjoy your wines in the future.
+        """+self.your_name+"""
+        (Thanks, Jennifer!)
+        """
+        }
+        
+        self.vegan_dict = {
+        "English": """Thanks for letting me know, I'm very happy to learn that your products are vegan! I'm looking forward to enjoying them.
+        Cheers,
+        """+your_name+"""
+        (Thanks, Tim!)
+        """
+        }
+        
+        self.set_language(language)
+
+    def set_language(self, language):
+        self.the_question_text = self.the_question_dict.get(language).strip().replace("        ","")
+        self.non_vegan_response = self.non_vegan_dict.get(language).strip().replace("        ","")
+        self.vegan_response = self.vegan_dict.get(language).strip().replace("        ","")
+
+    def set_brand_name(self, new_brand_name):
+        self.the_question_text = self.the_question_text.replace(self.brand_name, new_brand_name)
+        self.brand_name = new_brand_name
+
+    def set_your_name(self, new_name):
+        self.the_question_text = self.the_question_text.replace(self.your_name, new_name)
+        self.vegan_response = self.vegan_response.replace(self.your_name, new_name)
+        self.non_vegan_response = self.non_vegan_response.replace(self.your_name, new_name)
+        self.your_name = new_name
+
 
 class MobileAppPage:
 
