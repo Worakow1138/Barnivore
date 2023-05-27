49f55e30d175f14cb47a89c4cfe39b0c2232a3df
Christopher Diamond-Jones <christopher.jones1138@gmail.com>
2023-05-27 16:36:34 -0400

    Update Barnivore_Test_Suite_Desktop.py

--- Barnivore_Test_Suite_Desktop.py
+++ Barnivore_Test_Suite_Desktop.py
@@ -50,7 +50,7 @@ class BasicPageTests(BarnivoreTests):
         self.page_url_check(self.beer_page.header_title)
         self.filter_widget_checks(self.beer_page.filter_widget)
         self.listing_products_check(self.beer_page.list_widget)
-        self.mini_search_elements_check()
+        self.mini_search_elements_check(self.beer_page.search_widget)
         self.footer_checks()
 
     @Moonrise.test
@@ -59,7 +59,7 @@ class BasicPageTests(BarnivoreTests):
         self.page_url_check(self.cider_page.header_title)
         self.filter_widget_checks(self.cider_page.filter_widget)
         self.listing_products_check(self.cider_page.list_widget)
-        self.mini_search_elements_check()
+        self.mini_search_elements_check(self.cider_page.search_widget)
         self.footer_checks()
 
     @Moonrise.test
@@ -68,7 +68,7 @@ class BasicPageTests(BarnivoreTests):
         self.page_url_check(self.wine_page.header_title)
         self.filter_widget_checks(self.wine_page.filter_widget)
         self.listing_products_check(self.wine_page.list_widget)
-        self.mini_search_elements_check()
+        self.mini_search_elements_check(self.wine_page.search_widget)
         self.footer_checks()
 
     @Moonrise.test
@@ -77,7 +77,7 @@ class BasicPageTests(BarnivoreTests):
         self.page_url_check(self.liquor_page.header_title)
         self.filter_widget_checks(self.liquor_page.filter_widget)
         self.listing_products_check(self.liquor_page.list_widget)
-        self.mini_search_elements_check()
+        self.mini_search_elements_check(self.liquor_page.search_widget)
         self.footer_checks()
 
     @Moonrise.test
@@ -85,7 +85,7 @@ class BasicPageTests(BarnivoreTests):
         self.load_page(self.askacompany_page.header_title)
         self.page_url_check(self.askacompany_page.header_title)
         self.company_text_checks(self.askacompany_page)
-        self.mini_search_elements_check()
+        self.mini_search_elements_check(self.askacompany_page.search_widget)
         self.footer_checks()
 
     @Moonrise.test
@@ -93,7 +93,7 @@ class BasicPageTests(BarnivoreTests):
         self.load_page(self.mobile_page.header_title)
         self.page_url_check(self.mobile_page.header_title)
         self.mobile_text_checks()
-        self.mini_search_elements_check()
+        self.mini_search_elements_check(self.mobile_page.search_widget)
         self.footer_checks()
 
     @Moonrise.test
@@ -101,7 +101,7 @@ class BasicPageTests(BarnivoreTests):
         self.load_page(self.contact_page.header_title)
         self.page_url_check(self.contact_page.header_title)
         self.contact_text_checks()
-        self.mini_search_elements_check()
+        self.mini_search_elements_check(self.contact_page.search_widget)
         self.footer_checks()
 
     @Moonrise.test
@@ -109,5 +109,5 @@ class BasicPageTests(BarnivoreTests):
         self.load_page(self.faq_page.header_title)
         self.page_url_check(self.faq_page.header_title)
         self.faq_text_checks()
-        self.mini_search_elements_check()
         self.footer_checks()  
+
