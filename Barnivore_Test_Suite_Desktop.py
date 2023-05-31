721a480fb8390e53f4bcc9903136e8a452d41df9
Christopher Diamond-Jones <christopher.jones1138@gmail.com>
2023-05-31 16:32:23 -0400

    Update Barnivore_Test_Suite_Desktop.py

--- Barnivore_Test_Suite_Desktop.py
+++ Barnivore_Test_Suite_Desktop.py
@@ -1,6 +1,7 @@
 from moonrise import Moonrise
 from Barnivore_Test_Steps import BarnivoreTestSteps
 from Barnivore_Elements import *
+from typing import Union
 
 
 class BarnivoreTests(BarnivoreTestSteps):
@@ -16,6 +17,29 @@ class BarnivoreTests(BarnivoreTestSteps):
     def suite_teardown(self):
         self.cleanup_browser()
         return super().suite_teardown()
+    
+    def filter_test_steps(self, page: Union[BeerPage, CiderPage, WinePage, LiquorPage], filter: str, country: str = None, vegan_only: bool = False):
+        self.click_element(f"link:{filter}")
+        if vegan_only:
+            self.click_element(f"link:{page.filter_widget.only_vegan_filter}")
+        else:
+            self.click_element(f"link:{page.filter_widget.everything_filter}")
+
+        products = self.get_web_elements(page.list_widget.list_items)
+
+        assert len(products) <= 50
+        if filter != page.filter_widget.all_filter:
+            self.results_are_within_filtered_range(page.list_widget, filter, products)
+        if vegan_only:
+            self.results_are_vegan(page.list_widget, products)
+        self.filtered_headers_check(page, filter, country, vegan_only)
+        self.list_in_alphabetical_order(page.list_widget, products)
+        if country:
+            self.products_from_country(page.list_widget, country, products)
+        self.results_have_correct_labels(page.list_widget, products)
+
+        self.search_elements_check(page.search_widget, find_booze=True)
+        self.footer_checks()
     
     
 class BasicPageTests(BarnivoreTests):
@@ -244,3 +268,273 @@ class ProductEvaluationTests(BarnivoreTests):
         self.results_have_correct_labels(self.search_page.list_widget, self.get_web_elements(self.search_page.list_widget.list_items))
         self.search_elements_check(self.search_page.search_widget, find_booze=True)
         self.footer_checks()
+
+class BeerPageTestsUSA(BarnivoreTests):
+
+    def suite_setup(self):
+        self.beer_page = BeerPage()
+        self.country = "USA"
+        return super().suite_setup()
+    
+    def test_setup(self):
+        self.navigate_to_page("barnivore.com")
+        self.load_page(self.beer_page.header_title)
+        self.filter_widget_checks(self.beer_page.filter_widget)
+        self.select_from_list_by_label(self.beer_page.filter_widget.region_element, self.country)
+        return super().test_setup()
+    
+    @Moonrise.test
+    def all_filter(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.all_filter, self.country)
+
+    @Moonrise.test
+    def all_filter_vegan(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.all_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def zero_nine_filter(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.zero_nine_filter, self.country)
+
+    @Moonrise.test
+    def zero_nine_filter_vegan(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.zero_nine_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def a_f_filter(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.a_f_filter, self.country)
+
+    @Moonrise.test
+    def a_f_filter_vegan(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.a_f_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def g_l_filter(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.g_l_filter, self.country)
+
+    @Moonrise.test
+    def g_l_filter_vegan(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.g_l_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def m_r_filter(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.m_r_filter, self.country)
+
+    @Moonrise.test
+    def m_r_filter_vegan(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.m_r_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def s_t_filter(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.s_t_filter, self.country)
+
+    @Moonrise.test
+    def s_t_filter_vegan(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.s_t_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def u_z_filter(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.u_z_filter, self.country)
+
+    @Moonrise.test
+    def u_z_filter_vegan(self):
+        self.filter_test_steps(self.beer_page, self.beer_page.filter_widget.u_z_filter, self.country, vegan_only=True)
+
+class CiderPageTestsUSA(BarnivoreTests):
+
+    def suite_setup(self):
+        self.cider_page = CiderPage()
+        self.country = "USA"
+        return super().suite_setup()
+    
+    def test_setup(self):
+        self.navigate_to_page("barnivore.com")
+        self.load_page(self.cider_page.header_title)
+        self.filter_widget_checks(self.cider_page.filter_widget)
+        self.select_from_list_by_label(self.cider_page.filter_widget.region_element, self.country)
+        return super().test_setup()
+    
+    @Moonrise.test
+    def all_filter(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.all_filter, self.country)
+
+    @Moonrise.test
+    def all_filter_vegan(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.all_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def zero_nine_filter(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.zero_nine_filter, self.country)
+
+    @Moonrise.test
+    def zero_nine_filter_vegan(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.zero_nine_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def a_f_filter(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.a_f_filter, self.country)
+
+    @Moonrise.test
+    def a_f_filter_vegan(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.a_f_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def g_l_filter(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.g_l_filter, self.country)
+
+    @Moonrise.test
+    def g_l_filter_vegan(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.g_l_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def m_r_filter(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.m_r_filter, self.country)
+
+    @Moonrise.test
+    def m_r_filter_vegan(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.m_r_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def s_t_filter(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.s_t_filter, self.country)
+
+    @Moonrise.test
+    def s_t_filter_vegan(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.s_t_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def u_z_filter(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.u_z_filter, self.country)
+
+    @Moonrise.test
+    def u_z_filter_vegan(self):
+        self.filter_test_steps(self.cider_page, self.cider_page.filter_widget.u_z_filter, self.country, vegan_only=True)
+
+class WinePageTestsUSA(BarnivoreTests):
+
+    def suite_setup(self):
+        self.wine_page = WinePage()
+        self.country = "USA"
+        return super().suite_setup()
+    
+    def test_setup(self):
+        self.navigate_to_page("barnivore.com")
+        self.load_page(self.wine_page.header_title)
+        self.filter_widget_checks(self.wine_page.filter_widget)
+        self.select_from_list_by_label(self.wine_page.filter_widget.region_element, self.country)
+        return super().test_setup()
+    
+    @Moonrise.test
+    def all_filter(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.all_filter, self.country)
+
+    @Moonrise.test
+    def all_filter_vegan(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.all_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def zero_nine_filter(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.zero_nine_filter, self.country)
+
+    @Moonrise.test
+    def zero_nine_filter_vegan(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.zero_nine_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def a_f_filter(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.a_f_filter, self.country)
+
+    @Moonrise.test
+    def a_f_filter_vegan(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.a_f_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def g_l_filter(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.g_l_filter, self.country)
+
+    @Moonrise.test
+    def g_l_filter_vegan(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.g_l_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def m_r_filter(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.m_r_filter, self.country)
+
+    @Moonrise.test
+    def m_r_filter_vegan(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.m_r_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def s_t_filter(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.s_t_filter, self.country)
+
+    @Moonrise.test
+    def s_t_filter_vegan(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.s_t_filter, self.country, vegan_only=True)
+
+    @Moonrise.test
+    def u_z_filter(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.u_z_filter, self.country)
+
+    @Moonrise.test
+    def u_z_filter_vegan(self):
+        self.filter_test_steps(self.wine_page, self.wine_page.filter_widget.u_z_filter, self.country, vegan_only=True)
+
+class LiquorPageTestsUSA(BarnivoreTests):
+
+    def suite_setup(self):
+        self.liquor_page = LiquorPage()
+        return super().suite_setup()
+    
+    def test_setup(self):
+        self.navigate_to_page("barnivore.com")
+        self.load_page(self.liquor_page.header_title)
+        self.filter_widget_checks(self.liquor_page.filter_widget)
+        return super().test_setup()
+
+    @Moonrise.test
+    def zero_nine_filter(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.zero_nine_filter)
+
+    @Moonrise.test
+    def zero_nine_filter_vegan(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.zero_nine_filter, vegan_only=True)
+
+    @Moonrise.test
+    def a_f_filter(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.a_f_filter)
+
+    @Moonrise.test
+    def a_f_filter_vegan(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.a_f_filter, vegan_only=True)
+
+    @Moonrise.test
+    def g_l_filter(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.g_l_filter)
+
+    @Moonrise.test
+    def g_l_filter_vegan(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.g_l_filter, vegan_only=True)
+
+    @Moonrise.test
+    def m_r_filter(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.m_r_filter)
+
+    @Moonrise.test
+    def m_r_filter_vegan(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.m_r_filter, vegan_only=True)
+
+    @Moonrise.test
+    def s_t_filter(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.s_t_filter)
+
+    @Moonrise.test
+    def s_t_filter_vegan(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.s_t_filter, vegan_only=True)
+
+    @Moonrise.test
+    def u_z_filter(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.u_z_filter)
+
+    @Moonrise.test
+    def u_z_filter_vegan(self):
+        self.filter_test_steps(self.liquor_page, self.liquor_page.filter_widget.u_z_filter, vegan_only=True)
